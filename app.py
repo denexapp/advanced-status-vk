1e304f1bf9fd952fde55933f5b3b6ecaf6a7ffd8
Denis Mukhametov <denis.mukhametov@gmail.com>
2018-03-17 05:14:24 +0500

    Changed main loop for bot debugging

--- app.py
+++ app.py
@@ -8,39 +8,41 @@ from vk import Vk
 
 
 def main_loop():
-    last_fm = LastFm(keys.last_fm_username, keys.last_fm_password, keys.last_fm_api_key, keys.last_fm_shared_secret)
-    vk = Vk(keys.vk_access_token)
-    default_status = None
-    old_track_status = ""
-    while True:
-        track = last_fm.get_last_fm_now_playing(keys.last_fm_username)
-        if track:
-            print('Now playing track:{}'.format(track.name))
-            new_track_status = 'Слушает {} - {} через Яндекс Музыку'.format(track.artist, track.name)
-            if default_status is None:
-                default_status = vk.get_status()
-                print('Default status is none, setting default: {}'.format(default_status))
-                vk.set_status(new_track_status)
-                print('Setting new status on vk: {}'.format(new_track_status))
-                old_track_status = new_track_status
-                print('Setting old status: {}'.format(old_track_status))
-            else:
-                print('Default status: {}'.format(default_status))
-                if old_track_status != new_track_status:
-                    print('Old track status differs from new, setting to: {}'.format(new_track_status))
-                    old_track_status = new_track_status
-                    vk.set_status(new_track_status)
-                else:
-                    print('Old track status and new are the same: {}'.format(new_track_status))
-        else:
-            print('No now playing track')
-            if default_status:
-                print('Default status exist, setting it to vk: {}'.format(default_status))
-                vk.set_status(default_status)
-                default_status = None
-            else:
-                print('Default status is None')
-        time.sleep(10)
+    # last_fm = LastFm(keys.last_fm_username, keys.last_fm_password, keys.last_fm_api_key, keys.last_fm_shared_secret)
+    # vk = Vk(keys.vk_access_token)
+    # default_status = None
+    # old_status = ""
+    # while True:
+    #     track = last_fm.get_last_fm_now_playing(keys.last_fm_username)
+    #     if track:
+    #         # print('Now playing track:{}'.format(track.name))
+    #         new_status = 'Слушает {} - {} через Яндекс Музыку'.format(track.artist, track.name)
+    #         if default_status is None:
+    #             default_status = vk.get_status()
+    #             # print('Default status is none, setting default: {}'.format(default_status))
+    #             vk.set_status(new_status)
+    #             # print('Setting new status on vk: {}'.format(new_status))
+    #             old_status = new_status
+    #             # print('Setting old status: {}'.format(old_status))
+    #         else:
+    #             # print('Default status: {}'.format(default_status))
+    #             if old_status != new_status:
+    #                 # print('Old track status differs from new, setting to: {}'.format(new_status))
+    #                 old_status = new_status
+    #                 vk.set_status(new_status)
+    #             else:
+    #                 # print('Old track status and new are the same: {}'.format(new_status))
+    #     else:
+    #         print('No now playing track')
+    #         if default_status:
+    #             # print('Default status exist, setting it to vk: {}'.format(default_status))
+    #             vk.set_status(default_status)
+    #             default_status = None
+    #         else:
+    #             # print('Default status is None')
+    #     time.sleep(10)
+    vk = Vk(keys.vk_group_id, keys.vk_group_access_token)
+    vk.start_bot()
 
 if __name__ == '__main__':
     main_loop()
