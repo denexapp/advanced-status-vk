013f76861ca48bb361a98fe4ff63816ef7325eb8
Denis Mukhametov <denis.mukhametov@gmail.com>
2018-04-13 02:07:22 +0500

    Move BotUser class to BotData

--- app/botdata.py
+++ app/botdata.py
@@ -1,12 +1,16 @@
 from app.basedata import BaseData
-from app.botuser import BotUser
 
 
 class BotData(BaseData):
+    class BotUser:
+        def __init__(self, user_id: str, token: str = None):
+            self.user_id = user_id  # type: str
+            self.vk_token = token  # type: str
+
     def update_user(self, user_id: str, token: str = None):
         if token:
             self._users[user_id].vk_token = token
 
     def add_user(self, user_id: str, token: str = None):
-        user = BotUser(user_id, token=token)
+        user = BotData.BotUser(user_id, token=token)
         self._users[user_id] = user
